7bf227dfae9008d7012c2c878c68a56af1839ebc
ithar <itharm@ext.inditex.com>
2020-05-14 12:41:17 +0100

    Initial commit

---
+++ src/main/java/com/malik/ithar/ecommercedemoapp/EcommerceDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.malik.ithar.ecommercedemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceDemoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/malik/ithar/ecommercedemoapp/EcommerceDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malik.ithar.ecommercedemoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
